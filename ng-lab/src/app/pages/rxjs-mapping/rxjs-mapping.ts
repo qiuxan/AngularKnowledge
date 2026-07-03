4ca9a3844d8aa21f1efd44fe491c48a33953f4fc
qiuxan <qiuxan@gmail.com>
2026-07-04 08:32:10 +1000

    RxJS Mapping Operators

--- ng-lab/src/app/pages/rxjs-mapping/rxjs-mapping.ts
+++ ng-lab/src/app/pages/rxjs-mapping/rxjs-mapping.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, signal } from '@angular/core';
+import { concatMap, delay, mergeMap, Subject, Subscription, switchMap, tap, of } from 'rxjs';
+
+type MappingMode = 'switchMap' | 'mergeMap' | 'concatMap';
 
 @Component({
   selector: 'app-rxjs-mapping',
@@ -6,4 +9,68 @@ import { Component } from '@angular/core';
   templateUrl: './rxjs-mapping.html',
   styleUrl: './rxjs-mapping.scss',
 })
-export class RxjsMapping {}
+export class RxjsMapping implements OnDestroy {
+  mode = signal<MappingMode>('switchMap');
+  logs = signal<string[]>([]);
+
+  private click$ = new Subject<number>();
+  private subscription?: Subscription;
+  private requestId = 1;
+
+  constructor() {
+    this.setupStream('switchMap');
+  }
+
+  runRequest() {
+    const id = this.requestId;
+    this.requestId += 1;
+
+    this.addLog(`click ${id}`);
+    this.click$.next(id);
+  }
+
+  setMode(mode: MappingMode) {
+    this.mode.set(mode);
+    this.logs.set([]);
+    this.requestId = 1;
+    this.setupStream(mode);
+  }
+
+  clearLogs() {
+    this.logs.set([]);
+  }
+
+  ngOnDestroy() {
+    this.subscription?.unsubscribe();
+  }
+
+  private getMappingOperator(mode: MappingMode) {
+    switch (mode) {
+      case 'switchMap':
+        return switchMap;
+      case 'mergeMap':
+        return mergeMap;
+      case 'concatMap':
+        return concatMap;
+    }
+  }
+
+  private setupStream(mode: MappingMode) {
+    this.subscription?.unsubscribe();
+
+    const operator = this.getMappingOperator(mode);
+
+    this.subscription = this.click$
+      .pipe(operator((id) => this.fakeRequest(id).pipe(tap(() => this.addLog(`response ${id}`)))))
+      .subscribe();
+  }
+
+  private fakeRequest(id: number) {
+    this.addLog(`start request ${id}`);
+    return of(id).pipe(delay(1000));
+  }
+
+  private addLog(message: string) {
+    this.logs.update((logs) => [...logs, `${new Date().toLocaleTimeString()} - ${message}`]);
+  }
+}
