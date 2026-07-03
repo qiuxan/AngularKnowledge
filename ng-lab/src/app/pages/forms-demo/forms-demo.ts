86c2c4a50d288c791e3f403e6f7d7d7b0c618722
qiuxan <qiuxan@gmail.com>
2026-07-04 08:49:04 +1000

    Form Validate

--- ng-lab/src/app/pages/forms-demo/forms-demo.ts
+++ ng-lab/src/app/pages/forms-demo/forms-demo.ts
@@ -1,9 +1,46 @@
 import { Component } from '@angular/core';
+import { AbstractControl, ReactiveFormsModule, ValidationErrors, Validators, NonNullableFormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-forms-demo',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms-demo.html',
   styleUrl: './forms-demo.scss',
 })
-export class FormsDemo {}
+export class FormsDemo {
+  form;
+
+  constructor(private FormBuilder: NonNullableFormBuilder) {
+    this.form = this.FormBuilder.group(
+      {
+        name: ['', [Validators.required, Validators.minLength(2)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', [Validators.required]],
+      },
+      {
+        validators: [this.passwordsMatchValidator],
+      },
+    );
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    console.log('form value:', this.form.getRawValue());
+  }
+
+  private passwordsMatchValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    if (password !== confirmPassword) {
+      return { passwordsMismatch: true };
+    }
+
+    return null;
+  }
+}
