0bffd7a9bcb27be55bef430953c06f422b4997d0
qiuxan <qiuxan@gmail.com>
2026-07-05 08:59:34 +1000

    Finish Ng Redux ngrx demo

--- ng-lab/src/app/pages/ngrx-demo/ngrx-demo.ts
+++ ng-lab/src/app/pages/ngrx-demo/ngrx-demo.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectCounter, selectDoubleCounter } from '../../state/counter.selectors';
+import { decrement, increment, reset } from '../../state/counter.actions';
+import { AsyncPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-ngrx-demo',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './ngrx-demo.html',
   styleUrl: './ngrx-demo.scss',
 })
-export class NgrxDemo {}
+export class NgrxDemo {
+  private store = inject(Store);
+
+  count$ = this.store.select(selectCounter);
+  doubleCount$ = this.store.select(selectDoubleCounter);
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
+}
